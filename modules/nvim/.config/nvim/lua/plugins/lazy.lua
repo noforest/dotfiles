b7c009c4051d5ff081eedf0824ef3c94d871e3dd
noforest <noahforest1@gmail.com>
2026-08-26 19:52:02 +0200

    updates

--- modules/nvim/.config/nvim/lua/plugins/lazy.lua
+++ modules/nvim/.config/nvim/lua/plugins/lazy.lua
@@ -1333,50 +1333,34 @@ require("lazy").setup({
         },
     },
     -- {
-    --     "3rd/image.nvim",
-    --     build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-    --     config = function ()
-    --         require("image").setup({
-    --             backend = "kitty", -- or "ueberzug" or "sixel"
-    --             integrations = {
-    --                 markdown = {
-    --                     enabled = true,
-    --                     clear_in_insert_mode = false,
-    --                     download_remote_images = true,
-    --                     only_render_image_at_cursor = false,
-    --                     only_render_image_at_cursor_mode = "popup", -- or "inline"
-    --                     floating_windows = false, -- if true, images will be rendered in floating markdown windows
-    --                     filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    --                 },
-    --                 neorg = {
-    --                     enabled = true,
-    --                     filetypes = { "norg" },
-    --                 },
-    --                 typst = {
-    --                     enabled = true,
-    --                     filetypes = { "typst" },
-    --                 },
-    --                 html = {
-    --                     enabled = false,
-    --                 },
-    --                 css = {
-    --                     enabled = false,
-    --                 },
-    --             },
-    --             max_width = 100,
-    --             max_height = 12,
-    --             max_width_window_percentage = math.huge,
-    --             max_height_window_percentage = math.huge,
-    --             scale_factor = 1.0,
-    --             window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-    --             window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-    --             editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-    --             tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-    --             hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-    --         })
-    --     end
+    --   "3rd/image.nvim",
+    --   opts = {
+    --     backend = "kitty",
+    --     integrations = {
+    --       markdown = {
+    --         enabled = true,
+    --         clear_in_insert_mode = false,
+    --         download_remote_images = true,
+    --         only_render_image_at_cursor = false,
+    --         filetypes = { "markdown", "vimwiki" },
+    --       },
+    --     },
+    --     max_width = 100,
+    --     max_height = 12,
+    --     max_width_window_percentage = math.huge,
+    --     max_height_window_percentage = math.huge,
+    --
+    --     -- CONFIGURATION POUR ÉVITER LA DISPARITION AVEC BLINK.CMP :
+    --     window_overlap_clear_enabled = false, -- Empêche d'effacer l'image quand une popup s'ouvre
+    --     window_overlap_clear_ft_ignore = { 
+    --       "cmp_menu", 
+    --       "cmp_docs", 
+    --       "blink-cmp-documentation", 
+    --       "blink-cmp-menu", 
+    --       "" 
+    --     },
+    --   },
     -- },
-
 
     -- {
     --     "karb94/neoscroll.nvim",
@@ -1535,7 +1519,8 @@ require("lazy").setup({
 
     {
         'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons', '3rd/image.nvim' },
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 
         opts = {
             heading = {
@@ -1547,7 +1532,10 @@ require("lazy").setup({
                 enabled = true,
                 width = 'block',
                 sign = false,
-            }
+            },
+            image = {
+                enabled = false,
+            },
 
         },
     },
