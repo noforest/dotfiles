47a773a8273502e5a04c5b7f2ace62abb31d9ec3
noforest <noahforest1@gmail.com>
2026-09-02 21:05:18 +0200

    updates: icons at the same distance of each other dwm bar

--- system/arch/root/usr/local/bin/checkupdates.sh
+++ system/arch/root/usr/local/bin/checkupdates.sh
@@ -13,12 +13,15 @@ case $BUTTON in
 esac
 
 if [[ -f $CACHE && $(find "$CACHE" -mmin -30) ]]; then
-    echo " $ICON $(cat $CACHE)⠀ "
+    # echo " $ICON $(cat $CACHE)⠀ "
+    echo " $ICON $(cat $CACHE)"
 else
     if [ -f $CACHE ]; then
-        echo " $ICON $(cat $CACHE)  "
+        # echo " $ICON $(cat $CACHE)  "
+        echo " $ICON $(cat $CACHE)"
     else
-        echo " $ICON ... "
+        # echo " $ICON ... "
+        echo " $ICON ..."
     fi
     (
         COUNT=$(checkupdates | wc -l)
